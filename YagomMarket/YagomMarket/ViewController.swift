be01b4a11fbd4e56e46cf87ac6ef9bc7bfe5722d
wongbingg <wb277@naver.com>
2022-11-14 15:07:27 +0900

    feat : 프로젝트 생성

---
+++ YagomMarket/YagomMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YagomMarket
+//
+//  Created by 이원빈 on 2022/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
