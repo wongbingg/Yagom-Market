bb1293296200964d42517529780a2796f03215a7
wongbingg <wb277@naver.com>
2022-11-17 22:20:48 +0900

    refactor : 구조체 "APIConfiguration" 수정
    - URLRequest 구성을 위한 프로퍼티 추가
    - 'makeURLRequest()' 메서드 생성
        - 각 요청의 경우에 맞게 URLRequest 를 반환
    - 'createPostBody()' 메서드 생성
        - 상품등록 요청 POST의 경우 multipart/form-data 구성을 반환
        - 이미지를 데이터로 변환하는 'convertImages()' 메서드 구현
        - 각 데이터를 boundary 기준으로 나누기 위한 하나의 data단위를 반환하는 메서드 구현

--- YagomMarket/Networking/APIConfiguration.swift
+++ YagomMarket/Networking/APIConfiguration.swift
@@ -5,7 +5,7 @@
 //  Created by 이원빈 on 2022/11/14.
 //
 
-import Foundation
+import UIKit
 
 enum HTTPMethod: String {
     case get = "GET"
@@ -15,37 +15,114 @@ enum HTTPMethod: String {
 }
 
 enum URLCommand {
-    static let host = "https://openmarket.yagom-academy.kr/"
+    static let host = "https://openmarket.yagom-academy.kr"
     static let healthChecker = "healthChecker"
-    static let products = "api/products"
+    static let products = "/api/products"
+    static let identifier = "c08f22e6-5f28-11ed-a917-e3ffa43330f7"
+    static let secretKey = "nvjb13rd8y76lkzv2"
 }
 
 struct APIConfiguration {
     let method: HTTPMethod
+    var parameters: [String: Any]?
+    var body: ProductModel?
+    var images: [UIImage?]?
     var url: URL?
     
-    init(method: HTTPMethod,
-         base: String,
-         path: String,
-         parameters: [String : String]?) {
+    init(
+        method: HTTPMethod,
+        base: String,
+        path: String,
+        body: ProductModel? = nil,
+        parameters: [String : Any]?,
+        images: [UIImage?]? = nil
+    ) {
         self.method = method
+        self.parameters = parameters
+        self.body = body
+        self.images = images
         self.url = makeURL(base: base, path: path, parameters: parameters)
     }
     
-    private func makeQuery(with dic: [String: String]?) -> [URLQueryItem]? {
-        guard let dic = dic else { return nil }
-        var list: [URLQueryItem] = []
-        for (index, value) in dic {
-            list.append(URLQueryItem(name: index, value: value))
-        }
-        return list
+    init() {
+        self.method = .get
     }
     
     private func makeURL(base: String,
                          path: String,
-                         parameters: [String: String]?) -> URL? {
-        guard var urlComponent = URLComponents(string: base + path) else { return nil }
+                         parameters: [String: Any]?) -> URL? {
+        var urlComponent = URLComponents(string: base)!
+        urlComponent.path = path
         urlComponent.queryItems = makeQuery(with: parameters)
         return urlComponent.url
+    }
+    
+    private func makeQuery(with dic: [String: Any]?) -> [URLQueryItem]? {
+        guard let dic = dic else { return nil }
+        var list: [URLQueryItem] = []
+        for (index, value) in dic {
+            if let value = value as? String {
+                list.append(URLQueryItem(name: index, value: value))
+            } else if let value = value as? Int {
+                list.append(URLQueryItem(name: index, value: String(value)))
+            } else if let value = value as? Double {
+                list.append(URLQueryItem(name: index, value: String(value)))
+            }
+        }
+        return list
+    }
+    
+    func makeURLRequest() -> URLRequest? {
+        guard let url = url else { return nil }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method.rawValue
+        guard let body = body else { return urlRequest }
+        let boundary = UUID().uuidString
+        let postBody = createPostBody(with: body, at: boundary)
+        urlRequest.httpBody = postBody
+        urlRequest.setValue(
+            URLCommand.identifier,
+            forHTTPHeaderField: "identifier"
+        )
+        urlRequest.setValue(
+            "multipart/form-data; boundary=\(boundary)",
+            forHTTPHeaderField: "Content-Type"
+        )
+        return urlRequest
+    }
+    
+    func createPostBody(with body: ProductModel, at boundary: String) -> Data? {
+        var data = Data()
+        guard let paramData = try? JSONEncoder().encode(body),
+              let images = images else { return nil }
+        let startBoundaryData = "\r\n--\(boundary)\r\n"
+        data.appendString(startBoundaryData)
+        data.appendString("Content-Disposition: form-data; name=\"params\"\r\n\r\n")
+        data.append(paramData)
+        data.append(convertImages(images, using: boundary))
+        data.appendString("\r\n--\(boundary)--\r\n")
+        return data
+    }
+    
+    private func convertImages(_ images: [UIImage?], using boundary: String) -> Data {
+        var data = Data()
+        for image in images {
+            guard let image = image else { break }
+            let imageData = image.convertToData()
+            data.append(convertFileData(fieldName: "images", fileName: "abc",
+                                        mimeType: "image/jpeg", fileData: imageData, using: boundary))
+        }
+        return data
+    }
+    
+    private func convertFileData(fieldName: String, fileName: String,
+                                 mimeType: String, fileData: Data,using boundary: String) -> Data {
+        var data = Data()
+        data.appendString("\r\n--\(boundary)\r\n")
+        data.appendString("Content-Disposition: form-data; name=\"\(fieldName)\"; filename=\"\(fileName).png\"\r\n")
+        data.appendString("Content-Type: \(mimeType)\r\n\r\n")
+        data.append(fileData)
+        data.appendString("\r\n")
+        return data
     }
 }
