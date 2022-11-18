aa76e36a6727612bec8275331c5c97d85a745c76
wongbingg <wb277@naver.com>
2022-11-18 10:13:05 +0900

    test : "APIConfiguration" 테스트 수정 및 추가
    - 테스트 네임 수정
    - multipart/form-data 의 형식 확인을 위한 createPostBody 테스트 메서드 추가

--- YagomMarket/YagomMarketTests/APIConfigurationTest.swift
+++ YagomMarket/YagomMarketTests/APIConfigurationTest.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class APIConfigurationTest: XCTestCase {
     
-    func test_APIConfiguration으로_원하는url을생성할수있는지_1() {
+    func test_APIConfiguration으로_HealthChecker_url을생성할수있는지() {
         // given
         let expectationURL = URL(string: "https://openmarket.yagom-academy.kr/healthChecker")
         let expectationMethod: HTTPMethod = .get
@@ -26,26 +26,28 @@ final class APIConfigurationTest: XCTestCase {
         XCTAssertEqual(expectationMethod, resultMethod)
     }
     
-    func test_APIConfiguration으로_원하는url을생성할수있는지_2() {
+    func test_APIConfiguration으로_상품리스트조회_url을생성할수있는지() {
         // given
-        let expectationURL = URL(string: "https://openmarket.yagom-academy.kr/api/products?page_no=1&item_per_page=1")
+        let urlComponents = URLComponents(string: "https://openmarket.yagom-academy.kr/api/products?page_no=1&item_per_page=1")
+        let expextationQueryItems = Set(urlComponents?.queryItems ?? [])
         let expectationMethod: HTTPMethod = .get
         
-        let param = ["page_no":"1", "item_per_page":"1"]
+        let param = ["page_no": 1, "item_per_page": 1]
         let apiConfig = APIConfiguration(method: .get,
                                          base: URLCommand.host,
                                          path: URLCommand.products,
                                          parameters: param)
         // when
-        let resultURL = apiConfig.url
+        let resultComponent = URLComponents(string: apiConfig.url?.absoluteString ?? "")
+        let resultQueryItem = Set(resultComponent?.queryItems ?? [])
         let resultMethod = apiConfig.method
         
         // then
-        XCTAssertEqual(expectationURL, resultURL)
+        XCTAssertEqual(expextationQueryItems, resultQueryItem)
         XCTAssertEqual(expectationMethod, resultMethod)
     }
     
-    func test_APIConfiguration으로_원하는url을생성할수있는지_3() {
+    func test_APIConfiguration으로_상품상세조회_url을생성할수있는지() {
         // given
         let expectationURL = URL(string: "https://openmarket.yagom-academy.kr/api/products/179")
         let expectationMethod: HTTPMethod = .get
@@ -62,5 +64,26 @@ final class APIConfigurationTest: XCTestCase {
         // then
         XCTAssertEqual(expectationURL, resultURL)
         XCTAssertEqual(expectationMethod, resultMethod)
+    }
+    
+    func test_APIConfiguration의_createPostBody메서드_테스트() {
+        let model = ProductModel(name: "유닛테스트",
+                                 description: "테스트용 모델",
+                                 price: 100,
+                                 currency: .USD,
+                                 discountedPrice: nil,
+                                 stock: 4,
+                                 secret: URLCommand.secretKey)
+        let images = [UIImage(named: "Photo")]
+        let apiConfig = APIConfiguration(method: .get,
+                                         base: URLCommand.host,
+                                         path: URLCommand.products,
+                                         body: model,
+                                         parameters: nil,
+                                         images: images)
+        let boundary = UUID().uuidString
+        guard let data = apiConfig.createPostBody(with: model, at: boundary) else { return }
+        print(String(decoding: data, as: UTF8.self))
+        
     }
 }
