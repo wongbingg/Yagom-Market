775c389728954175376d05e36a71e7e13ad3403e
wongbingg <wb277@naver.com>
2022-11-24 23:12:41 +0900

    feat : "EditProductAPI" 생성
    - 상품수정을 위한 API 생성
    - 상품수정 PATCH 요청에 들어갈 body 타입인 "EditProductModel" 생성

---
+++ YagomMarket/YagomMarket/API/EditProductAPI.swift
@@ -0,0 +1,14 @@
+//
+//  EditProductAPI.swift
+//  YagomMarket
+//
+//  Created by 이원빈 on 2022/11/24.
+//
+
+import Foundation
+
+struct EditProductAPI: API {
+    typealias ResponseType = SearchProductDetailResponse
+    
+    var configuration: APIConfiguration
+}
