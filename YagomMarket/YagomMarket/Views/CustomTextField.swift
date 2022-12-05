d2a4b05b7d125037cc6ba19de18b4510993321d3
wongbingg <wb277@naver.com>
2022-12-05 23:24:08 +0900

    feat : "RegisterView" 등록화면 구현
    - 사진앨범과 연결하기 위해 UIImagePickerController 사용. 하지만 곧 deprecated된다고 함.
    [PHPickerController로 리팩토링 고려]
    - imageView를 버튼처럼 활용하기 위해 UITapGestureRecognizer를 이용.
    - 자주쓰이는 TextField속성을 객체화 하여 "CustomTextField"로 분리

---
+++ YagomMarket/YagomMarket/Views/CustomTextField.swift
@@ -0,0 +1,33 @@
+//
+//  CustomTextField.swift
+//  YagomMarket
+//
+//  Created by 이원빈 on 2022/12/05.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.font = UIFont.boldSystemFont(ofSize: 20)
+        self.placeholder = placeholder
+        self.heightAnchor.constraint(equalToConstant: 70).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let separator = UIBezierPath()
+        separator.move(to: CGPoint(x: 0, y: bounds.maxY))
+        separator.addLine(to: CGPoint(x: bounds.maxX, y: bounds.maxY))
+        separator.lineWidth = 2
+        UIColor.lightGray.setStroke()
+        separator.stroke()
+        separator.close()
+    }
+}
--- YagomMarket/YagomMarket/Views/RegisterViewController/RegisterViewController.swift
+++ YagomMarket/YagomMarket/Views/RegisterViewController/RegisterViewController.swift
@@ -9,24 +9,76 @@ import UIKit
 
 final class RegisterViewController: UIViewController {
     // MARK: Properties
-    let registerView = RegisterView()
+    private let registerView = RegisterView()
+    private let navigationBar: UINavigationBar = {
+        let navBar = UINavigationBar()
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        navBar.isTranslucent = true
+        navBar.barTintColor = UIColor.white
+        return navBar
+    }()
     
     // MARK: View LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
         setupInitialView()
+        setupNavigationBar()
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.delegate = self
+        view.addGestureRecognizer(tapGesture)
     }
     
     // MARK: Methods
     private func setupInitialView() {
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .white
         view.addSubview(registerView)
         registerView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            registerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            registerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
             registerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             registerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             registerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
+    }
+    
+    private func setupNavigationBar() {
+        view.addSubview(navigationBar)
+        NSLayoutConstraint.activate([
+            navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        let navItem = UINavigationItem(title: " ")
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismissView))
+        navItem.leftBarButtonItem = cancelButton
+        navigationBar.items = [navItem]
+    }
+    
+    @objc private func dismissView() {
+        self.dismiss(animated: true)
+    }
+}
+
+extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage { // 용량때문에 실제 post 안될수도..?
+            registerView.appendImage(image)
+        }
+        dismiss(animated: true)
+    }
+}
+
+extension RegisterViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        let position = touch.location(in: registerView)
+        if registerView.addPhotoButton.bounds.contains(position) {
+            let vc = UIImagePickerController()
+            vc.sourceType = .photoLibrary // will deprecate
+            vc.delegate = self
+            vc.allowsEditing = true
+            present(vc, animated: true)
+        }
+        return true
     }
 }
