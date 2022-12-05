829170f6a9a7d081c1d307e5dc3765fa7aba0388
wongbingg <wb277@naver.com>
2022-12-05 23:25:25 +0900

    feat : 사진앨범 접근권한 허용을 위한 Info.plist 추가

--- YagomMarket/YagomMarket/Entries/SceneDelegate.swift
+++ YagomMarket/YagomMarket/Entries/SceneDelegate.swift
@@ -17,7 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         
         let rootViewController = TabBarController()
-        
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
     }
