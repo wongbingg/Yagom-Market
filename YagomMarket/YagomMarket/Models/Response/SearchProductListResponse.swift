04cb4f58a4dcd072ce98951ee68a3ad778a4e93e
wongbingg <wb277@naver.com>
2022-12-06 23:37:13 +0900

    refactor : response 모델 수정

--- YagomMarket/YagomMarket/Models/Response/SearchProductListResponse.swift
+++ YagomMarket/YagomMarket/Models/Response/SearchProductListResponse.swift
@@ -8,27 +8,15 @@
 import Foundation
 
 struct SearchProductListResponse: Decodable {
-    let pageNo: Int?
-    let itemsPerPage: Int?
-    let totalCount: Int?
+    let pageNo: Int
+    let itemsPerPage: Int
+    let totalCount: Int
     let offset: Int
     let limit: Int
-    let lastPage: Int?
-    let hasNext: Bool?
-    let hasPrev: Bool?
+    let lastPage: Int
+    let hasNext: Bool
+    let hasPrev: Bool
     let pages: [Page]
-
-    enum CodingKeys: String, CodingKey {
-        case pageNo = "page_no"
-        case itemsPerPage = "items_per_page"
-        case totalCount = "total_count"
-        case offset
-        case limit
-        case lastPage = "last_page"
-        case hasNext = "has_next"
-        case hasPrev = "has_prev"
-        case pages
-    }
 }
 
 struct Page: Decodable {
