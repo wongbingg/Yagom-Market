86ddd9d8bb34181ed2c15015d17844ad6bdc119f
wongbingg <wb277@naver.com>
2022-12-07 10:22:09 +0900

    feat : "RegisterViewModel" 생성
    - 등록 버튼을 눌렀을 때, POST 요청을 보내는 비즈니스 로직을 viewModel을 통해 구현

--- YagomMarket/YagomMarket/ViewModels/RegisterViewModel.swift
+++ YagomMarket/YagomMarket/ViewModels/RegisterViewModel.swift
@@ -5,4 +5,47 @@
 //  Created by 이원빈 on 2022/11/29.
 //
 
-import Foundation
+import UIKit
+
+protocol RegisterViewModelInput {
+    func requestPost()
+}
+
+protocol RegisterViewModelOutput {
+    
+}
+
+protocol RegisterViewModel: RegisterViewModelInput, RegisterViewModelOutput {}
+
+final class DefaultRegisterViewModel: RegisterViewModel {
+    private var model: ProductModel?
+    private var images: [UIImage]?
+    
+    func adoptModel(with domain: ProductModel) {
+        model = domain
+    }
+    
+    func adoptImages(with imageArr: [UIImage]) {
+        images = imageArr
+    }
+    
+    func requestPost() {
+        let apiConfig = APIConfiguration(
+            method: .post,
+            base: URLCommand.host,
+            path: URLCommand.products,
+            body: model,
+            parameters: nil,
+            images: images
+        )
+        let api = RegisterProductAPI(configuration: apiConfig)
+        api.execute { result in
+            switch result {
+            case .success(_):
+                print("성공")
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
+    }
+}
