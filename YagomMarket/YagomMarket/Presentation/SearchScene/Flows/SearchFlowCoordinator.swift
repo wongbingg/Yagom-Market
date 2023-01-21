998a7fe8acca1e62d3eaaa3b54563b47f7679bc8
wongbingg <wb277@naver.com>
2023-01-21 11:12:53 +0900

    refactor : SearchFlowCoordinator 수정 #9
    - View Transition 추가

--- YagomMarket/YagomMarket/Presentation/SearchScene/Flows/SearchFlowCoordinator.swift
+++ YagomMarket/YagomMarket/Presentation/SearchScene/Flows/SearchFlowCoordinator.swift
@@ -9,6 +9,11 @@ import UIKit
 
 protocol SearchFlowCoordinatorDependencies: AnyObject {
     func makeSearchViewController(actions: SearchViewModelActions) -> SearchViewController
+    func makeResultViewController(model: ProductListResponseDTO, actions: ResultViewModelAction) -> ResultViewController
+    
+    func makeProductDetailViewController(productId: Int, actions: ProductDetailViewModelActions) -> ProductDetailViewController
+    func makeRegisterViewController(model: ProductDetail?, actions: RegisterViewModelActions) -> RegisterViewController
+    func makeImageViewerController(imageURLs: [String], currentPage: Int) -> ImageViewerViewController
 }
 
 final class SearchFlowCoordinator {
@@ -20,6 +25,10 @@ final class SearchFlowCoordinator {
     }
     
     func start() {
+//        let actions = SearchViewModelActions(goToResultVC: goToResultVC(with:))
+//        let searchVC = dependencies.makeSearchViewController(actions: actions)
+//        navCon.pushViewController(searchVC, animated: true)
+//        navigationController = UINavigationController(rootViewController: searchVC)
 //        let actions = ProductListViewModelActions(
 //            productTapped: productTapped(id:),
 //            anotherTabTapped: anotherTabTapped
@@ -27,16 +36,63 @@ final class SearchFlowCoordinator {
 //        let homeVC = dependencies.makeProductListViewController(actions: actions)
 //        navigationController.pushViewController(homeVC, animated: true)
     }
-    // test
+    
     func generate() -> UINavigationController? {
-        let actions = SearchViewModelActions(cellTapped: cellTapped(name:))
+        let actions = SearchViewModelActions(goToResultVC: goToResultVC(with:))
         let searchVC = dependencies.makeSearchViewController(actions: actions)
         navigationController = UINavigationController(rootViewController: searchVC)
         return navigationController
     }
     
     // MARK: View Transition
-    func cellTapped(name: String) {
-        // 검색결과로 이동
-    }    
+    func goToResultVC(with model: ProductListResponseDTO) {
+        let actions = ResultViewModelAction(
+            cellTapped: cellTapped(at:)
+        )
+        let resultVC = dependencies.makeResultViewController(
+            model: model,
+            actions: actions
+        )
+        navigationController?.pushViewController(resultVC, animated: true)
+    }
+    
+    func cellTapped(at id: Int) {
+        let actions = ProductDetailViewModelActions(
+            imageTapped: imageTapped(imageURLs:currentPage:),
+            showEditView: showEditView(model:)
+        )
+        let detailVC = dependencies.makeProductDetailViewController(productId: id, actions: actions)
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+    func imageTapped(imageURLs: [String], currentPage: Int) {
+        let imageViewerVC = dependencies.makeImageViewerController(
+            imageURLs: imageURLs,
+            currentPage: currentPage
+        )
+        // CustomModal 설정
+        navigationController?.topViewController?.present(imageViewerVC, animated: true)
+    }
+    
+    func showEditView(model: ProductDetail) {
+        let actions = RegisterViewModelActions(
+            registerButtonTapped: registerButtonTapped,
+            editButtonTapped: editButtonTapped
+        )
+        let editModalVC = dependencies.makeRegisterViewController(
+            model: model,
+            actions: actions
+        )
+//        editView.delegate = self
+        editModalVC.modalPresentationStyle = .overFullScreen
+        navigationController?.topViewController?.present(editModalVC, animated: true)
+    }
+    
+    func registerButtonTapped() {
+        print("등록버튼 탭") //dismiss 역할
+    }
+    
+    func editButtonTapped() {
+        print("수정버튼 탭") // dismiss 역할
+    }
 }
