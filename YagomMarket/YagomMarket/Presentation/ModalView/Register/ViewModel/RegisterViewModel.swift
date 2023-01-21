9ee833437d05b65cab0843b320efe6c08d2eae27
wongbingg <wb277@naver.com>
2023-01-21 11:23:19 +0900

    feat : RegisterViewModel 생성 #9
    - async-await 적용
    - actions 추가

---
+++ YagomMarket/YagomMarket/Presentation/ModalView/Register/ViewModel/RegisterViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  RegisterViewModel.swift
+//  YagomMarket
+//
+//  Created by 이원빈 on 2022/11/29.
+//
+
+struct RegisterViewModelActions {
+    let registerButtonTapped: () -> Void
+    let editButtonTapped: () -> Void
+}
+
+protocol RegisterViewModelInput {
+    func requestPost(with registerModel: RegisterModel) async
+    func requestPatch(with registerModel: RegisterModel) async
+    func registerButtonTapped(with registerModel: RegisterModel) async
+}
+
+protocol RegisterViewModelOutput {
+    var model: ProductDetail? { get }
+}
+
+protocol RegisterViewModel: RegisterViewModelInput, RegisterViewModelOutput {}
+
+final class DefaultRegisterViewModel: RegisterViewModel {
+    private(set) var model: ProductDetail?
+    private let actions: RegisterViewModelActions?
+    
+    init(model: ProductDetail? = nil, actions: RegisterViewModelActions? = nil) {
+        self.model = model
+        self.actions = actions
+    }
+    
+    func requestPost(with registerModel: RegisterModel) async {
+        let api = RegisterProductAPI(model: registerModel)
+        do {
+            _ = try await api.execute()
+        } catch {
+            // 에러처리
+            print(error.localizedDescription)
+        }
+    }
+    
+    func requestPatch(with registerModel: RegisterModel) async {
+        let editModel = registerModel.requestDTO.toEditRequestDTO()
+        let api = EditProductAPI(editModel: editModel, productId: model?.id ?? 0)
+        do {
+            _ = try await api.execute()
+        } catch {
+            print(error.localizedDescription)
+            // 오류얼럿
+        }
+    }
+    
+    func registerButtonTapped(with registerModel: RegisterModel) async {
+        if model == nil {
+            await requestPost(with: registerModel)
+            actions?.registerButtonTapped()
+        } else {
+            await requestPatch(with: registerModel)
+            actions?.editButtonTapped()
+        }
+    }
+}
