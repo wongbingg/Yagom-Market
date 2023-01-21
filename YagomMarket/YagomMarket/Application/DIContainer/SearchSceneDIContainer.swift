54fbfbe57b0d2b15121b4ef2dacd534b163672f5
wongbingg <wb277@naver.com>
2023-01-21 10:30:54 +0900

    refactor : SearchSceneDIContainer 수정 #9
    - SearchScene에 필요한 의존성을 생성하는 메서드 추가
    - ResultViewController 생성 메서드
    - ProductDetailViewController 생성 메서드
    - ModalView 생성 메서드

--- YagomMarket/YagomMarket/Application/DIContainer/SearchSceneDIContainer.swift
+++ YagomMarket/YagomMarket/Application/DIContainer/SearchSceneDIContainer.swift
@@ -18,6 +18,40 @@ final class SearchSceneDIContainer {
         return DefaultSearchViewModel(actions: actions)
     }
     
+    // MARK: - Result
+    func makeResultViewController(model: ProductListResponseDTO, actions: ResultViewModelAction) -> ResultViewController {
+        let viewModel = makeResultViewModel(model: model, actions: actions)
+        return ResultViewController(viewModel: viewModel)
+    }
+    
+    func makeResultViewModel(model: ProductListResponseDTO,actions: ResultViewModelAction) -> ResultViewModel {
+        return DefaultResultViewModel(model: model, actions: actions)
+    }
+    
+    // MARK: - Product Detail
+    func makeProductDetailViewController(productId: Int, actions: ProductDetailViewModelActions) -> ProductDetailViewController {
+        let viewModel = makeProductDetailViewModel(productId: productId, actions: actions)
+        return ProductDetailViewController(viewModel: viewModel)
+    }
+    
+    func makeProductDetailViewModel(productId: Int,
+                                    actions: ProductDetailViewModelActions) -> ProductDetailViewModel {
+        return DefaultProductDetailViewModel(actions: actions, productId: productId)
+    }
+    
+    // MARK: - Modal View
+    func makeRegisterViewController(model: ProductDetail?, actions: RegisterViewModelActions) -> RegisterViewController {
+        let viewModel = makeRegisterViewModel(model: model, actions: actions)
+        return RegisterViewController(with: viewModel)
+    }
+    
+    func makeRegisterViewModel(model: ProductDetail?, actions: RegisterViewModelActions) -> RegisterViewModel {
+        return DefaultRegisterViewModel(model: model, actions: actions)
+    }
+    
+    func makeImageViewerController(imageURLs: [String], currentPage: Int) -> ImageViewerViewController {
+        return ImageViewerViewController(imageURLs: imageURLs, currentPage: currentPage)
+    }
     // MARK: - UseCase
     
     // MARK: - Search Flow Coordinator
