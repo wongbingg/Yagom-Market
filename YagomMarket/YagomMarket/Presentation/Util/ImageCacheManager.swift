7048a1d4fbcb255d3d9461dc6790c9a627bca7e6
wongbingg <wb277@naver.com>
2023-01-26 20:29:37 +0900

    refactor : ImageCacheManager 수정
    - async-await 으로 비동기처리

--- YagomMarket/YagomMarket/Presentation/Util/ImageCacheManager.swift
+++ YagomMarket/YagomMarket/Presentation/Util/ImageCacheManager.swift
@@ -8,53 +8,43 @@
 import UIKit
 
 protocol ImageCacheManager {
-    func getImage(with imageURL: URL, _ completion: @escaping (UIImage) -> Void)
+    func getImage(with imageURL: URL) async throws -> Data
 }
 
-final class URLCacheManager: ImageCacheManager {
+final class DefaultImageCacheManager: ImageCacheManager {
     private let cache = URLCache.shared
-    private var dataTask: URLSessionDataTask?
     
-    func getImage(with imageURL: URL, _ completion: @escaping (UIImage) -> Void) {
-        let request = URLRequest(url: imageURL)
-        if self.cache.cachedResponse(for: request) != nil {
-            self.loadImageFromCache(with: imageURL) { image in
-                completion(image)
-            }
-        } else {
-            self.downloadImage(with: imageURL) { image in
-                completion(image)
-            }
-        }
-    }
-    
-    func loadImageFromCache(with imageURL: URL, _ completion: @escaping (UIImage) -> Void) {
+    func getImage(with imageURL: URL) async throws -> Data {
         let request = URLRequest(url: imageURL)
         
-        if let data = self.cache.cachedResponse(for: request)?.data,
-            let image = UIImage(data: data) {
-            completion(image)
+        if self.cache.cachedResponse(for: request) == nil {
+            return try await downloadImage(with: imageURL)
+        } else {
+            return try loadImageFromCache(with: imageURL)
         }
     }
     
-    func downloadImage(with imageURL: URL, _ completion: @escaping (UIImage) -> Void) {
+    func loadImageFromCache(with imageURL: URL) throws -> Data {
         let request = URLRequest(url: imageURL)
-
-        self.dataTask = URLSession.shared.dataTask(with: imageURL) { data, response, _ in
-            if let data = data {
-                let cachedData = CachedURLResponse(response: response!, data: data)
-                self.cache.storeCachedResponse(cachedData, for: request)
-                completion(UIImage(data: data)!)
-            }
+        
+        if let data = self.cache.cachedResponse(for: request)?.data {
+            return data
+        } else {
+            throw APIError.unknown
         }
-        self.dataTask?.resume()
     }
     
-//    func downloadImage(with imageURL: URL) async throws -> UIImage {
-//        let request = URLRequest(url: imageURL)
-//        let (data, response) = try await URLSession.shared.data(for: request)
-//        let cachedData = CachedURLResponse(response: response!, data: data)
-//        self.cache.storeCachedResponse(cachedData, for: request)
-//        return UIImage(data: data)!
-//    }
+    func downloadImage(with imageURL: URL) async throws -> Data {
+        let request = URLRequest(url: imageURL)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        let successRange = (200..<300)
+        
+        guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { throw APIError.unknown }
+        guard successRange.contains(statusCode) else { throw APIError.response(statusCode) }
+        
+        let cachedData = CachedURLResponse(response: response, data: data)
+        self.cache.storeCachedResponse(cachedData, for: request)
+        
+        return data
+    }
 }
