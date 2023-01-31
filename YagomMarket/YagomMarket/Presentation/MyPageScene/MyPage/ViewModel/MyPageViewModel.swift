f2c5804093475bd8a443f4ac27f089b58f0fad22
wongbingg <wb277@naver.com>
2023-01-31 18:04:32 +0900

    refactor : MyPageViewModel 수정 #17
    - 필요한 viewModelAction 과 UseCase 를 Coordinator 에서 주입받아 사용
    - 화면전환 로직에 스레드 오류가 발생하여 @MainActor 를 사용하여 MainThread 에서 실행됨을 보장

--- YagomMarket/YagomMarket/Presentation/MyPageScene/MyPage/ViewModel/MyPageViewModel.swift
+++ YagomMarket/YagomMarket/Presentation/MyPageScene/MyPage/ViewModel/MyPageViewModel.swift
@@ -8,19 +8,33 @@
 struct MyPageViewModelActions {
     let registerTapSelected: () -> Void
     let searchTapSelected: () -> Void
+    let logoutCellTapped: () -> Void
+    let likedListCellTapped: () -> Void
+    let myProductListCellTapped: (ProductListResponseDTO) -> Void
 }
 protocol MyPageViewModelInput {
     func searchTapSelected()
     func registerTapSelected()
+    func logoutCellTapped()
+    func likedListCellTapped()
+    func myProductListCellTapped() async throws
 }
 protocol MyPageViewModelOutput {}
 protocol MyPageViewModel: MyPageViewModelInput, MyPageViewModelOutput {}
 
 final class DefaultMyPageViewModel: MyPageViewModel {
     private let actions: MyPageViewModelActions
+    private let searchQueryResultsUseCase: SearchQueryResultsUseCase
+    private let searchUserProfileUseCase: SearchUserProfileUseCase
     
-    init(actions: MyPageViewModelActions) {
+    init(
+        actions: MyPageViewModelActions,
+        searchQueryResultsUseCase: SearchQueryResultsUseCase,
+        searchUserProfileUseCase: SearchUserProfileUseCase
+    ) {
         self.actions = actions
+        self.searchQueryResultsUseCase = searchQueryResultsUseCase
+        self.searchUserProfileUseCase = searchUserProfileUseCase
     }
     
     func searchTapSelected() {
@@ -29,5 +43,21 @@ final class DefaultMyPageViewModel: MyPageViewModel {
     
     func registerTapSelected() {
         actions.registerTapSelected()
+    }
+    
+    func logoutCellTapped() {
+        actions.logoutCellTapped()
+    }
+    
+    func likedListCellTapped() {
+        actions.likedListCellTapped()
+    }
+    
+    @MainActor
+    func myProductListCellTapped() async throws {
+        let userProfile = try await searchUserProfileUseCase.execute()
+        let searchKeyword = userProfile.vendorName
+        let response = try await searchQueryResultsUseCase.execute(keyword: searchKeyword)
+        actions.myProductListCellTapped(response)
     }
 }
