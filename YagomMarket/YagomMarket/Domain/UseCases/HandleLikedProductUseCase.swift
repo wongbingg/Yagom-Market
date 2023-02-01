77e614b6947b838a08f7c80f5f99545c38804752
wongbingg <wb277@naver.com>
2023-02-02 07:46:04 +0900

    feat : 상품에 좋아요를 누르는 UseCase 추가 #16

---
+++ YagomMarket/YagomMarket/Domain/UseCases/HandleLikedProductUseCase.swift
@@ -0,0 +1,43 @@
+//
+//  HandleLikedProductUseCase.swift
+//  YagomMarket
+//
+//  Created by 이원빈 on 2023/02/01.
+//
+
+import Foundation
+
+final class HandleLikedProductUseCase {
+    private let firestoreService: DefaultFirestoreService<UserProfile>
+    
+    init(
+        firestoreService: DefaultFirestoreService<UserProfile>
+    ) {
+        self.firestoreService = firestoreService
+    }
+    
+    func execute(with productId: Int, isAdd: Bool) async throws {
+        guard let userUID = LoginCacheManager.fetchPreviousInfo() else {
+            throw LoginCacheError.noPreviousInfo
+        }
+        
+        var userProfile = try await firestoreService.read(
+            collectionId: "UserProfile",
+            documentId: userUID,
+            entity: UserProfile()
+        )
+        
+        if isAdd {
+            userProfile.likedProductIds.append(productId)
+        } else {
+            let index = userProfile.likedProductIds.firstIndex(of: productId)!
+            userProfile.likedProductIds.remove(at: index)
+        }
+        
+        try await firestoreService.update(
+            collectionId: "UserProfile",
+            documentId: userUID,
+            to: userProfile
+        )
+    }
+}
