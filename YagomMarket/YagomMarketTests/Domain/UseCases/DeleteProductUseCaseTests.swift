d9990750d4dd9295d81aae91ea514282f54e3691
wongbingg <wb277@naver.com>
2023-02-02 11:16:32 +0900

    test : DeleteProductUseCaseTests 추가 #19

---
+++ YagomMarket/YagomMarketTests/Domain/UseCases/DeleteProductUseCaseTests.swift
@@ -0,0 +1,50 @@
+//
+//  DeleteProductUseCaseTests.swift
+//  YagomMarketTests
+//
+//  Created by 이원빈 on 2023/02/02.
+//
+
+import XCTest
+@testable import YagomMarket
+
+final class DeleteProductUseCaseTests: XCTestCase {
+
+    class ProductRepositoryMock: ProductsRepository {
+        var deleteMethodCallCount = 0
+        var passedProductId = 0
+        
+        func fetchProductsList(pageNumber: Int,
+                               itemPerPage: Int,
+                               searchValue: String?) async throws -> ProductListResponseDTO {
+            return ProductListResponseDTO.toMockData(hasNext: false)
+        }
+        
+        func fetchProductDetail(productId: Int) async throws -> ProductDetail {
+            return ProductDetail.toMockData()
+        }
+        
+        func editProductDetail(with editModel: ProductEditRequestDTO,
+                               productId: Int) async throws {}
+        
+        func deleteProduct(productId: Int) async throws {
+            deleteMethodCallCount += 1
+            passedProductId = productId
+        }
+    }
+    
+    func test_DeleteProductUseCase실행될때_ProductRepository의deleteProduct메서드가실행되는지() async throws {
+        // given
+        let expectationCallCount = 1
+        let expectationProductId = 10
+        let productRepository = ProductRepositoryMock()
+        let useCase = DeleteProductUseCase(productsRepository: productRepository)
+        
+        // when
+        try await useCase.execute(productId: expectationProductId)
+        
+        // then
+        XCTAssertEqual(expectationCallCount, productRepository.deleteMethodCallCount)
+        XCTAssertEqual(expectationProductId, productRepository.passedProductId)
+    }
+}
