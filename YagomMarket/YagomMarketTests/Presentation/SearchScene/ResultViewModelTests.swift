d94775fbdd3734ea5a713709cdbc1658c35b020f
wongbingg <wb277@naver.com>
2023-02-06 17:53:23 +0900

    style : 개행 수정

--- YagomMarket/YagomMarketTests/Presentation/SearchScene/ResultViewModelTests.swift
+++ YagomMarket/YagomMarketTests/Presentation/SearchScene/ResultViewModelTests.swift
@@ -13,13 +13,18 @@ final class ResultViewModelTests: XCTestCase {
     var passedId = 0
     
     override func setUpWithError() throws {
-        let viewModelAction = ResultViewModelAction(cellTapped: cellTapped)
+        let viewModelAction = ResultViewModelAction(
+            cellTapped: cellTapped
+        )
         
         func cellTapped(id: Int) {
             passedId = id
         }
         
-        sut = DefaultResultViewModel(cells: [ProductCell.stub()], actions: viewModelAction)
+        sut = DefaultResultViewModel(
+            cells: [ProductCell.stub(id: 123)],
+            actions: viewModelAction
+        )
     }
 
     override func tearDownWithError() throws {
