1ec98c3ae44912fc1a57cf854be47b0e107b5188
wongbingg <wb277@naver.com>
2023-02-06 17:51:49 +0900

    test : RegisterViewModelTests 추가 #19

---
+++ YagomMarket/YagomMarketTests/Presentation/ModalView/RegisterViewModelTests.swift
@@ -0,0 +1,82 @@
+//
+//  RegisterViewModelTests.swift
+//  YagomMarketTests
+//
+//  Created by 이원빈 on 2023/02/06.
+//
+
+import XCTest
+@testable import YagomMarket
+
+final class RegisterViewModelTests: XCTestCase {
+    private var sut: RegisterViewModel!
+    private var registerProductUseCaseMock: RegisterProductUseCaseMock!
+    private var editProductUseCaseMock: EditProductUseCaseMock!
+
+    override func setUpWithError() throws {
+        registerProductUseCaseMock = RegisterProductUseCaseMock()
+        editProductUseCaseMock = EditProductUseCaseMock()
+        sut = DefaultRegisterViewModel(
+            registerProductUseCase: registerProductUseCaseMock,
+            editProductUseCase: editProductUseCaseMock
+        )
+    }
+
+    override func tearDownWithError() throws {
+        registerProductUseCaseMock = nil
+        editProductUseCaseMock = nil
+        sut = nil
+    }
+    
+    func test_전달된모델이없을때_registerButton을탭하면_RegisterProductUseCase가실행되는지() async throws {
+        // given
+        let expectationRegisterCallCount = 1
+        let expectationEditCallCount = 0
+        
+        // when
+        let registerModel = RegisterModel(requestDTO: ProductPostRequestDTO.stub(), images: nil)
+        try await sut.registerButtonTapped(with: registerModel)
+        
+        // then
+        XCTAssertEqual(expectationRegisterCallCount, registerProductUseCaseMock.callCount)
+        XCTAssertEqual(expectationEditCallCount, editProductUseCaseMock.callCount)
+    }
+    
+    func test_전달된모델이있을때_registerButton을탭하면_EditProductUseCase가실행되는지() async throws {
+        // given
+        let expectationRegisterCallCount = 0
+        let expectationEditCallCount = 1
+        
+        let model = ProductDetail.stub()
+        sut = DefaultRegisterViewModel(
+            model: model,
+            registerProductUseCase: registerProductUseCaseMock,
+            editProductUseCase: editProductUseCaseMock
+        )
+        
+        // when
+        let registerModel = RegisterModel(requestDTO: ProductPostRequestDTO.stub(), images: nil)
+        try await sut.registerButtonTapped(with: registerModel)
+        
+        // then
+        XCTAssertEqual(expectationRegisterCallCount, registerProductUseCaseMock.callCount)
+        XCTAssertEqual(expectationEditCallCount, editProductUseCaseMock.callCount)
+    }
+}
+
+class RegisterProductUseCaseMock: RegisterProductUseCase {
+    var callCount = 0
+    
+    func execute(with registerModel: RegisterModel) async throws {
+        callCount += 1
+    }
+}
+
+class EditProductUseCaseMock: EditProductUseCase {
+    var callCount = 0
+    
+    func execute(with requestDTO: ProductEditRequestDTO,
+                 productId: Int) async throws {
+        callCount += 1
+    }
+}
