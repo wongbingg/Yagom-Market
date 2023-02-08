695a5f255ece1843bf7b2702981119b4561d579f
wongbingg <wb277@naver.com>
2023-02-08 21:57:30 +0900

    refactor : UserProfile 모델 메서드 수정 #24

--- YagomMarket/YagomMarket/Domain/Entities/UserProfile.swift
+++ YagomMarket/YagomMarket/Domain/Entities/UserProfile.swift
@@ -12,22 +12,27 @@ struct UserProfile: Entity {
     let vendorName: String
     let email: String
     var likedProductIds: [Int]
+    var chattingUUIDList: [String]
     
     func toDictionary() -> Dictionary<String, Any> {
-        return ["vendorName": vendorName, "email": email, "likedProductIds": likedProductIds]
+        return ["vendorName": vendorName,
+                "email": email,
+                "likedProductIds": likedProductIds,
+                "chattingUUIDList": chattingUUIDList]
     }
     
-    func toEntity(with documentSnapshot: DocumentSnapshot) -> UserProfile {
-        return .init(vendorName: documentSnapshot.get("vendorName") as! String,
-                     email: documentSnapshot.get("email") as! String,
-                     likedProductIds: documentSnapshot.get("likedProductIds") as! [Int])
-    }
-}
-
-extension UserProfile {
-    init () {
-        self.vendorName = ""
-        self.email = ""
-        self.likedProductIds = []
+    static func toEntity(with documentSnapshot: DocumentSnapshot) throws -> UserProfile {
+        guard let vendorName = documentSnapshot.get("vendorName") as? String,
+              let email = documentSnapshot.get("email") as? String,
+              let likedProductIds = documentSnapshot.get("likedProductIds") as? [Int],
+              let chattingUUIDList = documentSnapshot.get("chattingUUIDList") as? [String] else {
+            
+            throw FirestoreServiceError.failToRead
+        }
+        
+        return .init(vendorName: vendorName,
+                     email: email,
+                     likedProductIds: likedProductIds,
+                     chattingUUIDList: chattingUUIDList)
     }
 }
