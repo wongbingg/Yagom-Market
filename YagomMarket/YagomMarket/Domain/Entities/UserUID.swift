06d238227fe9d4979e1a232860660abc4374b530
wongbingg <wb277@naver.com>
2023-02-09 10:32:56 +0900

    feat : UserUID 도메인모델 추가 #24
    - Firestore에 [vendorName: userUID] 로 저장해놓을 value 타입

---
+++ YagomMarket/YagomMarket/Domain/Entities/UserUID.swift
@@ -0,0 +1,25 @@
+//
+//  UserUID.swift
+//  YagomMarket
+//
+//  Created by 이원빈 on 2023/02/09.
+//
+
+import FirebaseFirestore
+
+struct UserUID: Entity {
+    let userUID: String
+    
+    func toDictionary() -> Dictionary<String, Any> {
+        return ["userUID": userUID]
+    }
+    
+    static func toEntity(with documentSnapshot: DocumentSnapshot) throws -> UserUID {
+        
+        guard let userUID = documentSnapshot.get("userUID") as? String else {
+            throw FirestoreServiceError.failToRead
+        }
+        
+        return .init(userUID: userUID)
+    }
+}
