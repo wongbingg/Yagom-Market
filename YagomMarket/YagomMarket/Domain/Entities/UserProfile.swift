7f176c25dc9111748f63ef84885567b475bc65ce
wongbingg <wb277@naver.com>
2023-02-09 13:40:34 +0900

    refactor : UserProfile 모델 프로퍼티 추가 #24
    - identifier 와 secret 을 추가

--- YagomMarket/YagomMarket/Domain/Entities/UserProfile.swift
+++ YagomMarket/YagomMarket/Domain/Entities/UserProfile.swift
@@ -11,19 +11,26 @@ import FirebaseFirestore
 struct UserProfile: Entity {
     let vendorName: String
     let email: String
+    let identifier: String
+    let secret: String
     var likedProductIds: [Int]
     var chattingUUIDList: [String]
     
     func toDictionary() -> Dictionary<String, Any> {
         return ["vendorName": vendorName,
                 "email": email,
+                "identifier": identifier,
+                "secret": secret,
                 "likedProductIds": likedProductIds,
                 "chattingUUIDList": chattingUUIDList]
     }
     
     static func toEntity(with documentSnapshot: DocumentSnapshot) throws -> UserProfile {
+        
         guard let vendorName = documentSnapshot.get("vendorName") as? String,
               let email = documentSnapshot.get("email") as? String,
+              let identifier = documentSnapshot.get("identifier") as? String,
+              let secret = documentSnapshot.get("secret") as? String,
               let likedProductIds = documentSnapshot.get("likedProductIds") as? [Int],
               let chattingUUIDList = documentSnapshot.get("chattingUUIDList") as? [String] else {
             
@@ -32,6 +39,8 @@ struct UserProfile: Entity {
         
         return .init(vendorName: vendorName,
                      email: email,
+                     identifier: identifier,
+                     secret: secret,
                      likedProductIds: likedProductIds,
                      chattingUUIDList: chattingUUIDList)
     }
