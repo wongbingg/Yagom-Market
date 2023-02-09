80aa00356da381aa0a1fb067a3d6666b368eefbe
wongbingg <wb277@naver.com>
2023-02-09 13:38:11 +0900

    refactor : LoginCacheManager 수정 #24
    - UserDefault 에 userUID, identifier, secret 을 "%" 를 분기로 저장

--- YagomMarket/YagomMarket/Presentation/Util/LoginCacheManager.swift
+++ YagomMarket/YagomMarket/Presentation/Util/LoginCacheManager.swift
@@ -8,12 +8,17 @@
 import Foundation
 
 struct LoginCacheManager {
-    static func setNewLoginInfo(_ info: String) {
+    static func setNewLoginInfo(userUID: String, identifier: String, secret: String) {
+        let info = userUID + "%" + identifier + "%" + secret
         UserDefaults.standard.set(info, forKey: "loginInfo")
     }
     
-    static func fetchPreviousInfo() -> String? {
-        return UserDefaults.standard.string(forKey: "loginInfo")
+    static func fetchPreviousInfo() -> (userUID: String, identifier: String, secret: String)? {
+        guard let loginInfo = UserDefaults.standard.string(forKey: "loginInfo") else { return nil }
+        let userUID = String(loginInfo.split(separator: "%")[0])
+        let identifier = String(loginInfo.split(separator: "%")[1])
+        let secret = String(loginInfo.split(separator: "%")[2])
+        return (userUID, identifier, secret)
     }
     
     static func removeLoginInfo() {
