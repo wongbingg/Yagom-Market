1c2019a6e8572d3ca8f226ecf3362145dacd2c40
wongbingg <wb277@naver.com>
2023-02-11 23:18:20 +0900

    refactor : SearchOthersUIDUseCase 오류타입 정의 #24
    - 사용자가 서비스에 가입되어있지 않을 때 생기는 오류 정의

--- YagomMarket/YagomMarket/Domain/UseCases/SearchOthersUIDUseCase.swift
+++ YagomMarket/YagomMarket/Domain/UseCases/SearchOthersUIDUseCase.swift
@@ -5,11 +5,25 @@
 //  Created by 이원빈 on 2023/02/08.
 //
 
+import Foundation
+
 protocol SearchOthersUIDUseCase {
     func execute(with vendorName: String) async throws -> UserUID
 }
 
 final class DefaultSearchOthersUIDUseCase: SearchOthersUIDUseCase {
+    
+    private enum SearchError: LocalizedError {
+        case failToFindUser
+        
+        var errorDescription: String? {
+            switch self {
+            case .failToFindUser:
+                return "가입되지 않은 사용자 입니다."
+            }
+        }
+    }
+    
     private let firestoreService: any FirestoreService
     
     init(firestoreService: any FirestoreService) {
@@ -17,11 +31,14 @@ final class DefaultSearchOthersUIDUseCase: SearchOthersUIDUseCase {
     }
     
     func execute(with vendorName: String) async throws -> UserUID {
-        let response = try await firestoreService.read(
-            collectionId: "vendorNameToUserUID",
-            documentId: vendorName
-        )
-        
-        return response as! UserUID
+        do {
+            let response = try await firestoreService.read(
+                collectionId: "vendorNameToUserUID",
+                documentId: vendorName
+            )
+            return response as! UserUID
+        } catch {
+            throw SearchError.failToFindUser
+        }
     }
 }
