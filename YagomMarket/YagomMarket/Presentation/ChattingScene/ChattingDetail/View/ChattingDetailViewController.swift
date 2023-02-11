aa199a458cbacb011c0bb56d85d3a71694719d1c
wongbingg <wb277@naver.com>
2023-02-11 22:11:40 +0900

    feat : ChattingDetailViewController 생성 #24
    - ViewController 기본구현

---
+++ YagomMarket/YagomMarket/Presentation/ChattingScene/ChattingDetail/View/ChattingDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChattingDetailViewController.swift
+//  YagomMarket
+//
+//  Created by 이원빈 on 2023/02/09.
+//
+
+import UIKit
+
+final class ChattingDetailViewController: UIViewController {
+    
+}
