2e242525d1c7e22dbc39633801035459aa669c2d
wongbingg <wb277@naver.com>
2023-02-12 20:21:38 +0900

    refactor : ChattingDetailViewModel 수정 #24
    - 테스트 데이터 추가
    - fetchMessages() 와 sendMessages() 메서드 추가

--- YagomMarket/YagomMarket/Presentation/ChattingScene/ChattingDetail/ViewModel/ChattingDetailViewModel.swift
+++ YagomMarket/YagomMarket/Presentation/ChattingScene/ChattingDetail/ViewModel/ChattingDetailViewModel.swift
@@ -7,10 +7,60 @@
 
 import Foundation
 
-protocol ChattingDetailViewModelInput {}
-protocol ChattingDetailViewModelOutput {}
+protocol ChattingDetailViewModelInput {
+    func fetchMessages() async throws
+    func sendMessage(message: Message) async throws
+}
+
+protocol ChattingDetailViewModelOutput {
+    var testModel: [Message] { get }
+    var messages: [Message] { get }
+}
+
 protocol ChattingDetailViewModel: ChattingDetailViewModelInput, ChattingDetailViewModelOutput {}
 
 final class DefaultChattingDetailViewModel: ChattingDetailViewModel {
+    private(set) var messages: [Message] = []
+    private let chattingUUID: String
+    var testModel = [Message(body: "안녕하세요", sender: "bory", time: ""),
+                     Message(body: "네 안녕하세요", sender: "derrick", time: ""),
+                     Message(body: "물건관심있어 연락드려요", sender: "bory", time: ""),
+                     Message(body: "네 얼마 생각하세요?", sender: "derrick", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: ""),
+                     Message(body: "공짜여", sender: "bory", time: "")]
+    
+    private let searchChattingUseCase: SearchChattingUseCase
+    private let sendMessageUseCase: SendMessageUseCase
+    
+    init(
+        chattingUUID: String,
+        searchChattingUseCase: SearchChattingUseCase,
+        sendMessageUseCase: SendMessageUseCase
+    ) {
+        self.chattingUUID = chattingUUID
+        self.searchChattingUseCase = searchChattingUseCase
+        self.sendMessageUseCase = sendMessageUseCase
+    }
+    
+    func fetchMessages() async throws {
+        messages = try await searchChattingUseCase.execute(with: chattingUUID)
+    }
+    
+    func sendMessage(message: Message) async throws {
+        try await sendMessageUseCase.execute(
+            chattingUUID: chattingUUID,
+            message: message
+        )
+    }
     
 }
